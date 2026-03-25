9f2ba15afe8e3bdae5377c32b82f55323698a416
felip <felip@11.11.33.30>
2026-03-25 10:31:49 -0500

    creacion de la vista en consola y ejecucion de polimorfismo

---
+++ src/main/java/com/mycompany/herenciapolimorfismo10/HerenciaPolimorfismo10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herenciapolimorfismo10;
+
+/**
+ *
+ * @author felip
+ */
+public class HerenciaPolimorfismo10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
